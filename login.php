dcee59835efad9ff4c49270535f2780aa0ff8d8e
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-02-17 22:03:15 -0500

    created user input validation

--- login.php
+++ login.php
@@ -4,4 +4,14 @@
         Show a message if the username is "DIG" and the password is "3134".
         Show a different message if the username and/or password is incorrect.
     */
+
+    if (isset($_POST["username"]) && isset($_POST["password"]))
+    {
+        if ($_POST["username"] == "DIG" && $_POST["username"] == "3134")
+            echo("Correct username and password were inputted!");
+        else 
+            echo("Username and/or password is incorrect :(");
+    }
+    else   
+        echo("Enter values in each field please.");
 ?>
